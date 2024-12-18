bec3501299cb6f83d90f8055f633c88771c80dcd
jyzxc <867137498@qq.com>
2024-12-18 20:40:05 +0800

    fix: fix the paper service problem

--- src/main/java/usst/spm/service/impl/PapersServiceImpl.java
+++ src/main/java/usst/spm/service/impl/PapersServiceImpl.java
@@ -1,6 +1,7 @@
 package usst.spm.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
 import usst.spm.entity.Papers;
 import usst.spm.mapper.PapersMapper;
 import usst.spm.service.IPapersService;
@@ -11,6 +12,7 @@ import java.util.List;
  * @author jyzxc
  * @since 2024-11-26
  */
+@Service
 public class PapersServiceImpl extends ServiceImpl<PapersMapper, Papers> implements IPapersService {
     @Override
     public List<Papers> getPapersById(Integer courseId) {
